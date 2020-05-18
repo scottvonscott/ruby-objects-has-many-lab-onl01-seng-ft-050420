8d81ceef19e72fae5651064d78d6fb826608385a
Scott  Keplinger <skeplin17@gmail.com>
2020-05-18 19:26:28 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,28 @@
+class Song
+
+  attr_accessor :artist, :name, :genre
+
+  @@all = []
+
+  def initialize (name)
+    @name = name
+    save
+  end
+
+  def artist=(artist)
+    @artist = artist
+  end
+
+  def artist_name
+    self.artist.name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+end
